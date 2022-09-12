d9a2d7c588e9243ecfcd3c21766c5feed24e0035
mflavio- <mfghost69@gmail.com>
2022-09-13 01:24:28 +0200

    strjoin; modified substr

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/12 22:27:45 by coder             #+#    #+#             */
-/*   Updated: 2022/09/13 00:10:57 by coder            ###   ########.fr       */
+/*   Created: 2022/09/12 23:33:31 by coder             #+#    #+#             */
+/*   Updated: 2022/09/13 01:22:26 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		d_len;
 	char	*dest;
-	int		i;
+	size_t	s1_len;
+	size_t	s2_len;
 
-	d_len = len - start;
-	dest = (char *) malloc(sizeof(char) * (d_len + 1));
-	if (dest == NULL)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	dest = malloc(((s1_len + s2_len) * sizeof(char)) + 1);
+	if (dest)
 	{
-		return (NULL);
+		ft_strlcpy(dest, s1, s1_len + 1);
+		ft_strlcat(dest, s2, s1_len + s2_len + 1);
+		return (dest);
 	}
-	i = start;
-	while (i < len && (*(s + i) != '\0'))
-	{
-		*dest = *(s + i);
-		dest++;
-		i++;
-	}
-	*dest = '\0';
-	return (dest - d_len);
+	return (NULL);
 }
