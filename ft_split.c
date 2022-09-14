5baa014e6926191aeecb2e01fc4c03f26a1e27b5
mflavio- <mfghost69@gmail.com>
2022-09-15 01:10:48 +0200

    split commit inicial

--- ft_split.c
+++ ft_split.c
@@ -6,15 +6,65 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 00:06:02 by coder             #+#    #+#             */
-/*   Updated: 2022/09/14 22:17:42 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/15 01:10:03 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static size_t	get_words(char const *s, char c)
+{
+	size_t	words;
+	size_t	j;
+
+	words = 0;
+	j = 0;
+	while (*s)
+	{
+		if (*s != c && !j)
+		{
+			j = 1;
+			words++;
+		}
+		else if (*s == c)
+			j = 0;
+		s++;
+	}
+	return (i);
+}
+
+static size_t	special_strlen(char const *s, char c)
 {
 	size_t	i;
 
 	i = 0;
+	while (s[i] != c && s[i])
+		i++;
+	return (i);
+}
 
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	**mrx;
+
+	j = 0;
+	k = 0;
+	i = get_words(s, c);
+	mrx = (char **) ft_calloc(i + 1, sizeof(char *));
+	while (*s)
+	{
+		if (*s != c)
+		{
+			mrx[k] = ft_substr(s, 0, special_strlen(s, c));
+			s++;
+			k++;
+		}
+		while (*s != c && *s)
+			s++;
+		while (*s == c && *s)
+			s++;
+	}
+	return (mrx);
 }
