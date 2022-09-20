f31102b95a49b878e3efecaadcabdccaeaa53812
mflavio- <mfghost69@gmail.com>
2022-09-20 02:32:28 +0200

    Aparentemente commit final

--- ft_substr.c
+++ ft_substr.c
@@ -6,29 +6,24 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 22:27:45 by coder             #+#    #+#             */
-/*   Updated: 2022/09/16 02:46:52 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/20 02:22:15 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t		d_len;
-	char		*dest;
-	size_t		i;
+	char	*dest;
 
-	d_len = len - start;
-	dest = (char *) malloc(sizeof(char) * (d_len + 1));
-	if (!dest)
-	{
+	if (!s)
 		return (NULL);
-	}
-	i = start;
-	while (i < len && (*(s + i) != '\0'))
-	{
-		dest[i] = s[i];
-		i++;
-	}
-	dest[i] = '\0';
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	dest = (char *) malloc(sizeof(char) * len + 1);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s + start, len + 1);
 	return (dest);
 }
