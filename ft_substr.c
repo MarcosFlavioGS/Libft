e82d3f30bafb5d3f96955519021a5311354d5a46
mflavio- <mfghost69@gmail.com>
2022-09-23 01:37:48 +0200

    arrumando o substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: coder <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 22:27:45 by coder             #+#    #+#             */
-/*   Updated: 2022/09/20 02:22:15 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/23 01:20:47 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,6 +19,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (ft_strlen(s) < start)
 		len = 0;
+	if (s + start > s + ft_strlen(s))
+	{
+		dest = malloc(1);
+		*dest = '\0';
+		return (dest);
+	}
 	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	dest = (char *) malloc(sizeof(char) * len + 1);
